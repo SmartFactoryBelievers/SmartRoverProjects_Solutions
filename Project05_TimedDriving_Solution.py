c67cfbe01607f0f2d3a4965d53def0a33dc39ffe
CarsonLittle1026 <109241524+CarsonLittle1026@users.noreply.github.com>
2025-02-24 07:21:40 -0800

    Update Project05_TimedDriving_Solution.py
    updated solutions to match project code

--- Project05_TimedDriving_Solution.py
+++ Project05_TimedDriving_Solution.py
@@ -24,6 +24,8 @@ import RPi.GPIO as GPIO
 # We also now are using the general time library for the timer function
 import time
 
+GPIO.setwarnings(False)
+
 #Let's define variables so we can use them later
 Left_Forward_Pin = 35 #the internal Pi pin number that goes to snap 1
 Left_Backward_Pin = 31 #the internal Pi pin number that goes to snap 2
@@ -80,10 +82,10 @@ def drive_backward(time):
   
 # Here we are creating a timer function to record the duration of the button press
 def button_press_timer():
-  Start_Time = time.time() #start the timer
-  while GPIO.input(Button_Pin): #while the button is pressed...
-    print("Button Pressed")
-  return round(time.time() - Start_Time,2) #stop the timer, return elapsed time
+    Start_Time = time.time() #start the timer
+    while GPIO.input(Button_Pin): #while the button is pressed...
+        print("Button Pressed")
+    return round(time.time() - Start_Time,2) #stop the timer, return elapsed time
 
 # For challenges 3 and 4, we will use a dummy variable to help with modulo operator
 count = 0
@@ -92,20 +94,20 @@ count = 0
 # To use this as a logical, let's try count % 2 == 0
 
 while True: #Looping over and over again
-  sleep(0.25)
-  
-  # If the button is pressed, let's use the timer function to see how long
-  if GPIO.input(Button_Pin):
-    Button_Time = button_press_timer()
-    print('Button pressed ' + str(Button_Time) + ' seconds')
+    sleep(0.25)
+    
+    # If the button is pressed, let's use the timer function to see how long
+    if GPIO.input(Button_Pin):
+        Button_Time = button_press_timer()
+        print('Button pressed ' + str(Button_Time) + ' seconds')
     
     if count % 2 == 0: # Try changing the True to the modulo for challenges 3 and 4
-      
-      #For challenges 1 and 2, try adding new driving functions here
-      drive_forward(Button_Time)
+        #For challenges 1 and 2, try adding new driving functions here
+        drive_forward(Button_Time)
     
     else: # To be used in challenges 3 and 4
-      drive_backward(Button_Time)
-      # Add other drive functions here for odd button presses
+        drive_backward(Button_Time) # Add other drive functions here for odd button presses
       
-    count = count + 1 # We increment the counter for the next button press
+        count = count + 1 # We increment the counter for the next button press
+
+GPIO.cleanup()
